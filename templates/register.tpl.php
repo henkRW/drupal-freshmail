d1468fefb980537ff9bc43724d535fe94cdeba5a
Karol Bryksa <kb@ratioweb.pl>
2015-06-09 21:15:07 +0200

    Rename template

--- templates/register.tpl.php
+++
@@ -1,5 +0,0 @@
-<form class="freshmail" action="<?php print $action; ?>" method="post">
-  <input name="email" type="email" class="freshmail_input" placeholder="<?php print $placeholder; ?>"/>
-  <button><?php print $send; ?></button>
-  <div class="messages"></div>
-</form>
